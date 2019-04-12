ed6cdce05093cb0feaa21693d509f7dbf82de13c
ctyas <9556299+ctyas@users.noreply.github.com>
2019-04-12 10:56:41 +0100

    Add codecov to travis

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 import { ApolloProvider } from "react-apollo";
 
 import ApolloClient from "apollo-boost";
@@ -16,8 +15,3 @@ ReactDOM.render(
     <App />
 </ApolloProvider>,
 document.getElementById("root"));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
